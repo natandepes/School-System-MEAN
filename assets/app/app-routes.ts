b8bafd7e878c05b17a1ea03f048c1cdd206cf055
lubragon <lubragon@gmail.com>
2023-11-08 22:22:33 -0300

    Botão para area de edição a partir do  student está funcionando. Lembrar de acertar o CSS do student.

--- assets/app/app-routes.ts
+++ assets/app/app-routes.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from '../login/login.component';
 import { SignUpComponent } from '../sign-up/sign-up.component';
 import { StudentComponent } from '../student/student.component';
 import { SubjectComponent } from '../subject/subject.component';
+import { StudentEditComponent } from '../student/student-edit/student-edit.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
@@ -11,5 +12,7 @@ export const routes: Routes = [
   { path: 'message', component: MessageComponent },
   { path: 'aluno', component: StudentComponent },
   { path: 'materias', component: SubjectComponent },
+  {path: 'student-edit', component: StudentEditComponent},
   { path: '', redirectTo: '/login', pathMatch:'full' }
+ 
 ];
